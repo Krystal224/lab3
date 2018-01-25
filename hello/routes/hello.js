29382d891485b98cc0c26729d1ffc89c15cab01b
Ziwen Zeng <ziz236@ucsd.edu>
2018-01-25 11:58:34 -0800

    committed lab3

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -4,7 +4,16 @@
  */
 
 exports.view = function(req, res){
+
+  var nameToShow = req.params.userName;
+  if(nameToShow == undefined){
+  	nameToShow = 'Word';
+  }
+
+  console.log('Name is' + nameToShow);
   res.render('index', {
-  	'name': '',
+  	'name':nameToShow
   });
-};
+
+  
+}
